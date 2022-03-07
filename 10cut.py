c1837c7beae484fe353a33a20155a5d480030f91
CCCWS <eung0626@gmail.com>
2022-03-08 01:14:16 +0900

    cut

--- 10cut.py
+++ 10cut.py
@@ -1,3 +1,3 @@
 n = input()
-for i in range(0, len(n), 10) :
-    print(n[i:i+10])
+for i in range(0, len(n), 5) :
+    print(n[i:i+5])
---
+++ n pick.py
@@ -0,0 +1,5 @@
+n = int(input())
+a = list(range(1,n+1))
+# a.sort(reverse=True)
+for i in a :
+    print(i)
