02952e7f9d9cdd7047adf64e0ce49456c3099f3b
CCCWS <eung0626@gmail.com>
2022-03-09 04:05:28 +0900

    2XN

--- 2xN.py
+++ 2xN.py
@@ -1,12 +1,17 @@
 n = int(input())
 
 d = [0] * (n+1)
-d[1] = 1
-d[2] = 2
 
-for i in range(3, (n+1)) :
+for i in range(1, (n+1)) :
+    if i == 1 :
+        d[i] = 1
+        continue
+    elif i == 2 :
+        d[i] = 2
+        continue
     d[i] = d[i-1] + d[i-2]
     
-print (d[i] % 10007)
+print (d[n] % 10007)
 
 #피보나치 수열과 동일
+#d[1], d[2]를 fon문 밖에 미리 지정했을경우 런타임 에러 발생
