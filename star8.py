f8313fbf9c1e2586d89971f5bb8350912f2ffd60
CCCWS <eung0626@gmail.com>
2022-03-08 21:18:06 +0900

    star

--- star8.py
+++ star8.py
@@ -1,3 +1,6 @@
 n = int(input())
-for i in range(1, (n*2)) :
-    print('*' * i)
+for i in range(1, (n+1)) :
+    print('*' * i + ' ' * ((n*2)-(2*i)) + '*' * i)
+    
+for i in range((n-1), 0, -1) :
+    print('*' * i + ' ' * ((n*2)-(2*i)) + '*' * i)
