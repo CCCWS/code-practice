49f42fe0f1a9aa114b3afcaaf7775445d0c37a5a
CCCWS <eung0626@gmail.com>
2022-03-13 00:07:39 +0900

    https://www.acmicpc.net/problem/11722

---
+++ Dynamic Programming/감소하는 부분수열.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int, input().split()))
+d = [1] * n
+
+for i in range(1, n) : #1 ~ n-1
+    for j in range(i) : #0 ~ i-1
+        if a[i] < a[j] : #두 수를 비교하여 앞에값이 더 크면 수행X
+            d[i] = max(d[i], d[j]+1) #리스트 d의 값을 비교해가며 큰수로 갱신
+
+print (max(d)) #리스트 d중 가장 큰값을 출력 > 감소한 횟수
+
+#증가하는 부분수열과 동일한 문제
+#if문의 조건만 반대로 바꿔줌
