72beef8440abcfb9509290195420fd86737c24df
CWS <86645532+CCCWS@users.noreply.github.com>
2022-09-12 22:41:48 +0900

    Create 이진변환반복하기.js
    https://school.programmers.co.kr/learn/courses/30/lessons/70129

---
+++ JS/이진변환반복하기.js
@@ -0,0 +1,20 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/70129
+const solution = (s) => {
+  let arr = [...s];
+  let arrLength = 0;
+  let count = 0; //반복횟수
+  let zeroCount = 0; // 0제거 횟수
+
+  while (true) {
+    zeroCount = zeroCount + arr.filter((item) => "0" === item).length;
+    arrLength = arr.filter((s) => parseInt(s) !== 0).length;
+    arr = [...arrLength.toString(2)];
+    count++;
+    if (arr.length === 1) {
+      return [count, zeroCount];
+    }
+  }
+};
+
+const s = "1111111";
+console.log(solution(s));
